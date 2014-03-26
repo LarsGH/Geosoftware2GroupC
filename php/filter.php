44c40e59a305509a632cacdf8e72fb54a5fa6a58
LarsGH <lars.syfuss@googlemail.com>
2014-03-26 15:36:49 +0100

    array needs to be decoded

--- php/filter.php
+++ php/filter.php
@@ -94,7 +94,7 @@ else if  ($_POST["f"] == "getSpaceTimeTrack"){
 		$endtime = urldecode($_POST["endtime"]);
 	}
 	if(isset($_POST["weekday"])){
-		$weekday = $_POST["weekday"];
+		$weekday = json_decode($_POST["weekday"]);
 	}
 	if(isset($_POST["limit"])){
 		$limit = $_POST["limit"];
