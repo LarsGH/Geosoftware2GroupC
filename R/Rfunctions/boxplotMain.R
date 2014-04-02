881d6220804c5aa6c85ce96626c6a9f0018f9bd7
rolandhub <rolandh@uni-muenster.de>
2014-04-02 19:25:11 +0200

    Axis adjusted and data structure simplified

--- R/Rfunctions/boxplotMain.R
+++ R/Rfunctions/boxplotMain.R
@@ -26,20 +26,22 @@ boxplotMain <- function(layerJSON){
   if(length(layerJSON$tracks)>0){
     
     # Get the track from the list
-    track <- layerJSON$tracks[[1]]
+    track <- layerJSON$tracks
     
     # Check if track is populated with measurement points
     if (length(track$features) > 0){
       
-      # Get the track ID, date,  and model and manufacturer and  of the car
-      # to be used in the plot
+      # Get track-ID and model + manufacturer of the car
+      # to be used in the plot.
       trackId <- track$features[[1]]$properties$trackID
       model <- track$properties$sensor$properties$model
       manuf <- track$properties$sensor$properties$manufacturer
       car <- paste(manuf,model)
+      
+      # Get the date of the track in german format
       time <- track$features[[1]]$properties$time
       time <- as.POSIXct(time, format = "%Y-%m-%dT%H:%M:%SZ")
-      date <- as.Date(time)
+      date <- format(as.Date(time), "%d.%m.%Y")
       
       # Get the attribute values. One vector for each attribute.
       speedVec <- unlist(lapply(track$features, function(x) x$properties$phenomenons$Speed$value))
@@ -67,14 +69,51 @@ boxplotMain <- function(layerJSON){
                       Last = engLoadVec,
                       Ans.Druck = inPVec)
 
-      # Delete the NULL elements from the list of vectors
+      # Create a list containing the (german) named units vectors
+      unitsList <- list(Geschw. = "km/h",
+                        Verbr. = "l/h",
+                        Upm_100 = "100 u/min",
+                        CO2 = "kg/h",
+                        Ber.MAF = "g/s",
+                        MAF = "l/s",
+                        Ans.Temp. = "c",
+                        Last = "%",
+                        Ans.Druck = "kPa")
+      
+      # Create a list containing the (german) names of the attributes to be used in the plot
+      gerNamesList <- list(Geschw. = "Geschw.",
+                           Verbr. = "Verbr.",
+                           Upm_100 = "Upm",
+                           CO2 = "CO2",
+                           Ber.MAF = "Ber.MAF",
+                           MAF = "MAF",
+                           Ans.Temp. = "Ans.Temp.",
+                           Last = "Last",
+                           Ans.Druck = "Ans.Druck")
+      
+      # Delete the NULL elements (no measurement values) from the lists of vectors
       ind <- sapply(vecList, is.null)
       vecList[ind] <- NULL
+      unitsList[ind] <- NULL
+      gerNamesList[ind] <- NULL
+      
+      # Get the length of the final vector list
+      len <- length(vecList)
+      
+      # Initialize the names vector to be used in the boxplot function as an argument
+      namesVec <- character(len)
+      
+      # Fill the names vector with a combination of the german name and the unit for each attribute vector
+      for (i in 1:len){
+        namesVec[i] <- paste(gerNamesList[[i]], "\n", unitsList[[i]], sep="")
+      }
       
       # Create the boxplot
       boxplot(x=vecList,
+              names = namesVec,
               main=paste("Verteilung der Attributwerte der ausgewählten Fahrt.\nFahrzeug: ",
-                         car,", Datum: ", date, ", Track-ID: ",trackId,".", sep="")
+                         car,", Datum: ", date, ", Fahrt-ID: ",trackId,".\n", sep=""),
+              sub="Nur die zur Verfügung stehenden Attribute werden dargestellt.\n"
       )
       
       flog.info("boxplotMain function finished successfully.#")
