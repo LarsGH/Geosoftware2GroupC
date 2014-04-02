2970814582f62333e5fdbd42336a294466b6712b
zimmp <peterzimmerhof@googlemail.com>
2014-04-02 14:35:17 +0200

    Avoid string input number text box

--- js/core.js
+++ js/core.js
@@ -1054,6 +1054,11 @@ var analyse = new function() {
 			}
 		});				
 		
+		$('.numbersOnly').keyup(function () {
+		    if (this.value != this.value.replace(/[^0-9]/g, '')) {
+		       this.value = this.value.replace(/[^0-9]/g, '');
+		    }
+		});
 	};
 
 	// Set the analyse values from the controls
